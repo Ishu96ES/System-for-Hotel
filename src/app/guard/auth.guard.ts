4b4649bd4ae0bc760fe6d3dc98a639b515f2a9fe
Rishi0721 <rishmikalavandi@gmail.com>
2019-04-08 09:51:12 +0530

    ui editings

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
+export class AuthGuard implements  CanActivate {
+  path: ActivatedRouteSnapshot[];  route: ActivatedRouteSnapshot;
+
   
 }
